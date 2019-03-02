4bbfae125bf840625b73a02795d2df3f0413610a
dowpm <mcdowpm@gmail.com>
2019-03-02 00:12:36 -0500

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -18,7 +18,7 @@ import React, { Component, PureComponent } from 'react';
   componentDidUpdate() {
     this.timer.current.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
   }
-
+  //with PureComponent we do not need shouldComponentUpdate since its not implemented
   // shouldComponentUpdate(nextProps, nextState) {
   //   if (this.state.time === nextState.time) {
   //     return false
